a20a334fd8e066dbdee703985a051eb0d1611008
Jasmin Sandhu <jasmin.sandhu@mni.thm.de>
2025-08-10 16:00:40 +0200

    Add pages

---
+++ frontend/src/app/pages/notebooks/notebooks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notebooks.component',
+  imports: [],
+  templateUrl: './notebooks.component.html',
+  styleUrl: './notebooks.component.css'
+})
+export class NotebooksComponent {
+
+}
