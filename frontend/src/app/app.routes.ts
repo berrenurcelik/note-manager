865867630dfac63bd224bf751cc193612dde174d
berrenurcelik <berre.nur.celik@mni.thm.de>
2025-08-27 18:59:57 +0200

    added readme and redirect to login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { NotebooksComponent } from './pages/notebooks/notebooks.component';
 import { NotesComponent } from './pages/notes/notes.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/notebooks', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'notebooks', component: NotebooksComponent },
   { path: 'notes', component: NotesComponent },
-  { path: 'notes/:notebookId', component: NotesComponent }
+  { path: 'notes/:notebookId', component: NotesComponent },
 ];
